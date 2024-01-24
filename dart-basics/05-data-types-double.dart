34dff21f5345bfb93e9398af6832bee43e9b386b
Sujeong Ji <suji.sujeongji@gmail.com>
2024-01-24 23:27:53 +0900

    05. data types - double

---
+++ dart-basics/05-data-types-double.dart
@@ -0,0 +1,10 @@
+// Arithmetic operations 
+void main() {
+  double number1 = 0.5;
+  double number2 = 2.5;
+
+  print(number1 + number2);
+  print(number1 - number2);
+  print(number1 * number2);
+  print(number1 / number2);
+}
