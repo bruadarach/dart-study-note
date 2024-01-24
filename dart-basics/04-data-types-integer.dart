c292c2033d3b823e70af76d3423128220110492a
Sujeong Ji <suji.sujeongji@gmail.com>
2024-01-24 23:27:45 +0900

    04. data types - integer

---
+++ dart-basics/04-data-types-integer.dart
@@ -0,0 +1,24 @@
+void main() {
+  int number1 = 10;
+  print(number1);
+}
+
+// Arithmetic operations 
+void main() {
+  int number1 = 2;
+  int number2 = 4;
+
+  print(number1 + number2);
+  print(number1 - number2);
+  print(number1 * number2);
+  print(number1 / number2);
+
+	print(number1 % 2);
+	number1++
+	number1--
+
+	number += 1
+	number -= 1
+	number *= 1
+	number /= 1
+}
