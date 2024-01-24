9f902892cd462ae5e58a3916ed68c7cae22e7479
Sujeong Ji <suji.sujeongji@gmail.com>
2024-01-25 00:42:10 +0900

    19. if statement

---
+++ dart-basics/19-if-statement.dart
@@ -0,0 +1,13 @@
+// if, else if, else
+
+void main() {
+  int number = 3;
+  
+  if (number % 3 == 0 ) {
+    print('remainder 0'); 
+  }  else if (number % 3 == 1) {
+    print('remainder 1');
+  } else {
+    print('remainder 2'); 
+  }
+}
