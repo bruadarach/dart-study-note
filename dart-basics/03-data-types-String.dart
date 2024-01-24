1c1b681c3b3bddc1dd3a4889da7441151692a90f
Sujeong Ji <suji.sujeongji@gmail.com>
2024-01-24 23:27:32 +0900

    03. data types - String

---
+++ dart-basics/03-data-types-String.dart
@@ -0,0 +1,14 @@
+// Dart is statically typed, which means you need to declare the type of a variable before using it.
+
+void main() {
+  String firstName = "Sujeong";
+  String lastName = "Ji";
+  
+  print(firstName);
+  print(lastName);
+}
+
+// Addition (only possible)
+print(firstName + ' ' + lastName); 
+print('${firstName} ${lastName}'); 
+print('$firstName $lastName'); // omit {} 
